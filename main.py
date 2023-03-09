dcce1c14ac8c80dac8fa0f33af67887a0cd5a183
codesilas <silas.duarte.h@gmail.com>
2023-03-09 16:36:54 -0600

    Configuration of database consults w/ sqlalchemy

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
 from config.database import Session, engine, Base
 from models.movie import Movie as MovieModel # alias para evitar confución con la otra clase "Movie"
+from fastapi.encoders import jsonable_encoder
 
 
 app = FastAPI()
@@ -78,17 +79,30 @@ def login(user: User):
 
 @app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
-    return JSONResponse(status_code=200, content=movies)
+    db = Session()
+    result = db.query(MovieModel).all()
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
 
 @app.get('/movies/{id}', tags=['movies'], response_model=Movie)
 def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
-    for item in movies:
-        if item["id"] == id:
-            return JSONResponse(content=item)
-    return JSONResponse(status_code=404, content=[])
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404, content={"message":"No se encontró"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
 
 @app.get('/movies/', tags=['movies'], response_model=List[Movie])
 def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.category == category).all()
+    if not result:
+        return JSONResponse(status_code=404, content={"message":"No se encontró"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+    
+    
+    
     data = [ item for item in movies if item['category'] == category ]
     return JSONResponse(content=data)
 
